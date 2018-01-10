f90a367c33e6ab8b847e4b273d643156d5d5e149
Shubham Chaudhary <shubham.bspdesign@gmail.com>
2018-01-10 09:21:47 +0530

    Page 104

--- jmad/albums/admin.py
+++ jmad/albums/admin.py
@@ -3,5 +3,9 @@ from django.contrib import admin
 # Register your models here.
 from .models import Album, Track
 
+class TrackAdmin(admin.ModelAdmin):
+	model = Track
+	list_display = ('album', 'name', 'track_number')
+
 admin.site.register(Album)
-admin.site.register(Track)
+admin.site.register(Track, TrackAdmin)
